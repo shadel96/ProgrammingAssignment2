0b82997250ce3786504b611e65fa9c19b6b234d1
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
+##This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  null <- NULL
+  set <- function(mat) {
+    x <<- mat
+    null <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) null <<- inverse
+  getInverse <- function() null
+  list(
+    get = get,
+    set = set,
+    getInverse = getInverse,
+    setinverse = setInverse)
 }
 
 
 ## Write a short comment describing this function
 
+##This function computes the inverse of the special "matrix" 
+##returned by makeCacheMatrix above. If the inverse has already 
+##been calculated (and the matrix has not changed), then the cachesolve 
+##should retrieve the inverse from the cache.
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  cachedInverse <- x$getinverse()
+  if(!is.null(cachedInverse)) {
+  return(cachedInverse)
+  }
+  data <- x$get()
+  cachedInverse <- solve(data, ...)
+  x$setinverse(cachedInverse)
+  cachedInverse
 }
